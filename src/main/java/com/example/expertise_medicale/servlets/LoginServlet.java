3456a26e6fe32145e6d6036cc1ca296ead86c4a1
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-10-09 15:16:00 +0100

    implémenter le servlet LoginServlet pour la gestion de l'authentification

---
+++ src/main/java/com/example/expertise_medicale/servlets/LoginServlet.java
@@ -0,0 +1,67 @@
+package com.example.expertise_medicale.servlets;
+
+import com.example.expertise_medicale.models.Patient;
+import com.example.expertise_medicale.models.User;
+import com.example.expertise_medicale.models.enums.Role;
+import com.example.expertise_medicale.services.AuthService;
+import com.example.expertise_medicale.services.PatientService;
+import com.example.expertise_medicale.services.UserService;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    private AuthService authService;
+    private PatientService patientService;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        UserService<User> userService = new UserService<>(User.class);
+        this.authService = new AuthService(userService);
+        this.patientService = new PatientService();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email =  request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User user = authService.login(email, password);
+
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            List<Patient> patients = patientService.findByToday();
+
+            if (user.getRole().equals(Role.GENERALISTE)) {
+                request.getSession().setAttribute("patients", patients);
+                request.getRequestDispatcher("/generaliste.jsp").forward(request, response);
+
+            } else if (user.getRole().equals(Role.INFIRMIER)) {
+                request.getSession().setAttribute("patients", patients);
+                request.getRequestDispatcher("/patients.jsp").forward(request, response);
+
+            } else if (user.getRole().equals(Role.SPECIALISTE)) {
+                response.sendRedirect(request.getContextPath() + "/specialiste.jsp");
+
+            } else {
+                request.setAttribute("error", "Role invalide");
+                request.getRequestDispatcher("/login.jsp").forward(request, response);
+            }
+        } else  {
+            request.setAttribute("error", "Identifiants incorrects");
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+
+
+    }
+}
