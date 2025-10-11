92215a219188b8270b31a1f099734ea917034c8b
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-10-11 12:00:00 +0100

    Améliorer UserDAO : ajout de EntityManager local dans toutes les méthodes pour plus de sécurité et d’isolation

--- src/main/java/com/example/expertise_medicale/dao/UserDAO.java
+++ src/main/java/com/example/expertise_medicale/dao/UserDAO.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 public class UserDAO<T> {
     public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("expertiseMedicale");
-    private EntityManager em;
     private final Class<T> entityClass;
 
     public UserDAO(Class<T> entityClass) {
         this.entityClass = entityClass;
-        em = emf.createEntityManager();
     }
 
     public void add(T t){
+        EntityManager em = emf.createEntityManager();
+
         em.getTransaction().begin();
         em.persist(t);
         em.getTransaction().commit();
@@ -25,6 +25,8 @@ public class UserDAO<T> {
     }
 
     public void update(T t){
+        EntityManager em = emf.createEntityManager();
+
         em.getTransaction().begin();
         em.merge(t);
         em.getTransaction().commit();
@@ -32,14 +34,18 @@ public class UserDAO<T> {
     }
 
     public T find(String id){
+        EntityManager em = emf.createEntityManager();
         return em.find(entityClass, id);
     }
 
     public void delete(T t){
+        EntityManager em = emf.createEntityManager();
         em.remove(t);
     }
 
     public List<T> findAll(){
+        EntityManager em = emf.createEntityManager();
+
         List<T> list = em.createQuery("SELECT e FROM " + entityClass.getSimpleName() + " e", entityClass).getResultList();
         return list;
     }
