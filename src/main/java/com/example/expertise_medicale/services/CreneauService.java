db1beed2764859fb9ba99c4ae5787e018a0bc419
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-10-12 10:42:00 +0100

    Créer CreneauService avec gestion des créneaux disponibles par spécialiste

---
+++ src/main/java/com/example/expertise_medicale/services/CreneauService.java
@@ -0,0 +1,34 @@
+package com.example.expertise_medicale.services;
+
+import com.example.expertise_medicale.dao.CreneauDAO;
+import com.example.expertise_medicale.models.Creneau;
+
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+public class CreneauService {
+    private CreneauDAO creneauDAO = new CreneauDAO();
+
+    public void add(Creneau creneau) {
+        creneauDAO.add(creneau);
+    }
+
+    public List<Creneau> getAllCreneau(){
+        return creneauDAO.findAll();
+    }
+
+    public List<Creneau> findBySpecialiste(Long specialisteId){
+        return getAllCreneau().stream().filter(c -> c.getSpecialiste().getId().equals(specialisteId)).toList();
+    }
+
+    public List<Creneau> getCreneauxDisponibles(Long specialisteId) {
+        return findBySpecialiste(specialisteId)
+                .stream()
+                .filter(Creneau::getDisponible)
+                .collect(toList());
+    }
+
+    public  Creneau getCreneauById(Long id){
+        return getAllCreneau().stream().filter(c -> c.getId().equals(id)).findFirst().get();
+    }
+}
