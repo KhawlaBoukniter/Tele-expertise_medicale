1a7e63b0fdb308fc7be89af11cd34259d756e729
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-10-12 15:40:00 +0100

    Modifier ActeMedicalService : ajouter méthode getActesByConsultation pour récupérer les actes liés à une consultation

--- src/main/java/com/example/expertise_medicale/services/ActeMedicalService.java
+++ src/main/java/com/example/expertise_medicale/services/ActeMedicalService.java
@@ -3,10 +3,16 @@ package com.example.expertise_medicale.services;
 import com.example.expertise_medicale.dao.ActeMedicalDAO;
 import com.example.expertise_medicale.models.ActeMedical;
 
+import java.util.List;
+
 public class ActeMedicalService {
     private ActeMedicalDAO acteMedicalDAO = new ActeMedicalDAO();
 
     public void add(ActeMedical acte) {
         acteMedicalDAO.add(acte);
+    }
+
+    public List<ActeMedical> getActesByConsultation(Long id) {
+        return acteMedicalDAO.findAll().stream().filter(a -> a.getConsultation().getId().equals(id)).toList();
     }
 }
